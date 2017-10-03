d80d38a7198063a48512414fdc64c50e5a6c4a2f
Christine Cha <ccha@squarespace.com>
2017-10-02 21:08:57 -0400

    Refreshed home view

--- src/scripts/components/HomeView.js
+++ src/scripts/components/HomeView.js
@@ -1,42 +1,95 @@
+
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { signInWithFacebook, getUserFromFacebook } from '../db/auth'
+import { signInWithFacebook } from '../db/auth'
 
 class HomeView extends React.Component {
-  constructor ( props ) {
+  constructor( props ) {
     super( props )
 
-    this.signInWithFacebook = this.signInWithFacebook.bind( this )
-
-    this.getUserFromFacebook()
+    this.state = {
+      carouselIndex: 0,
+    }
   }
 
-  getUserFromFacebook () {
-    getUserFromFacebook()
+  componentDidMount() {
+    this.$slides = this.$carousel.children
+    this.numOfSlides = this.$slides.length
+
+    this.carouselInterval = setInterval(() => {
+      this.nextSlide()
+    }, 3000 )
   }
 
-  signInWithFacebook () {
-    signInWithFacebook()
+  componentWillUnmount() {
+    clearInterval( this.carouselInterval )
+  }
+
+  nextSlide() {
+    const carouselIndex = ( this.state.carouselIndex + 1 ) % this.numOfSlides
+    this.setState({ carouselIndex })
+  }
+
+  renderCarousel() {
+    const slides = [
+      '',
+      '',
+      '',
+      '',
+    ]
+
+    const $slides = slides.map(( slide, i ) => {
+      const isActive = this.state.carouselIndex === i
+      return (
+        <div className={`slide ${ isActive ? 'is-active' : '' }`} key={i}></div>
+      )
+    })
+
+    const $indicators = slides.map(( slide, i ) => {
+      const isActive = this.state.carouselIndex === i
+      return (
+        <div className={`indicator ${ isActive ? 'is-active' : '' }`} key={i}></div>
+      )
+    })
+
+    return (
+      <div
+        className='onboarding-carousel'
+        ref={( $c ) => this.$carousel = $c}
+      >
+        <div className='slides-wrapper'>
+          {$slides}
+        </div>
+        <div className='indicators-wrapper'>
+          {$indicators}
+        </div>
+      </div>
+    )
   }
 
   render () {
     return (
       <main id='home-view' className='view'>
-        <h1>Tastemark</h1>
+        <div className='view-content'>
+          <h1>tastemark</h1>
+          <h3>The best way to save the best places.</h3>
+          {this.renderCarousel()}
+          <button
+            className='button facebook full-width'
+            onClick={signInWithFacebook}
+          >
+            Sign up with Facebook
+          </button>
+          <Link to='/signup'>
+            <button className='button full-width knockout'>
+              Sign up with Email
+            </button>
+          </Link>
 
-        <button className='button facebook full-width' onClick={this.signInWithFacebook}>
-          Continue With Facebook
-        </button>
-
-        <hr />
-
-        <Link to='/signup'>
-          <button className='button full-width knockout'>Sign Up With Email</button>
-        </Link>
-
-        <div className='log-in-prompt'>
-          Already have an account?
-          <Link to='/login' className='log-in'>Log In</Link>
+          <div className='log-in-prompt small'>
+            Already have an account?
+            <Link to='/login' className='log-in'>Log In</Link>
+          </div>
         </div>
       </main>
     )
